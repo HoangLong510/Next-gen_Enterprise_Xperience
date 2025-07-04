a119c45189a3931a32df9d1f1158b9c1e0951996
hoanggiahuy <huypg7645@gmail.com>
2025-07-04 12:04:51 +0700

    trash

--- client/src/utils/function.js
+++
@@ -1,9 +0,0 @@
-export const formatDate = (dateString, language) => {
-	return new Date(dateString).toLocaleDateString(
-		language === "vi" ? "vi-VN" : "en-US",
-		{
-			year: "numeric",
-			month: "long",
-			day: "numeric",
-		})
-}
